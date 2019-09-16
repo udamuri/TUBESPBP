c88840120cbedd5b30aea46207e66b90cf65d786
niusindra <38039465+niusindra@users.noreply.github.com>
2019-09-16 16:05:58 +0700

    Splash screen

--- app/src/main/java/com/example/tubespbp/MainActivity.java
+++ app/src/main/java/com/example/tubespbp/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.tubespbp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int waktu_loading=3000;
+
+    //4000=4 detik
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(MainActivity.this, Home.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
     }
 }
--- app/src/main/java/com/example/tubespbp/asdf.java
+++
@@ -1,5 +0,0 @@
-package com.example.tubespbp;
-
-public class asdf {
-    permisi;
-}
